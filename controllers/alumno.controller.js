3e917811d303a65f98c4cbb6899cc0aae5e109fe
ValentinDePascale <valentindp9@gmail.com>
2026-05-29 13:16:17 -0300

    feat: add alumno POST route and express JSON middleware

--- controllers/alumno.controller.js
+++ controllers/alumno.controller.js
@@ -40,4 +40,39 @@ const getAlumnoById = async (req, res) => {
   }
 };
 
-module.exports = { getAlumnoAll, getAlumnoById };
+/*const postAlumno = async (req, res) => {
+  {
+  const { nombre, apellido, email } = req.body;
+
+  const data = await fs.readFile("./data/alumnos.json", "utf8");
+  const alumnos = JSON.parse(data);
+
+  console.log('Se pareseo infomracion a "alumnos"');
+  const legajos = alumnos.map((alumno) => alumno.legajo);
+  const newLegajo = Math.max(...legajos) + 1;
+  console.log("Nuevo legajo generado");
+
+   const nuevoAlumno = new AlumnoModel(nombre, apellido, email, nuevoLegajo);
+
+    console.log(nuevoAlumno);
+    const alumnoNuevo = nuevoAlumno.getAllAtributes();
+    alumno.push(alumnoNuevo);
+    console.log(nuevoAlumno.getAllAttributes());
+
+    fs.writeFile(
+      ".data/alumnos.json",
+      JSON.stringify(alumnoNuevo, null, 2),
+      "utf8"
+    );
+    return res.status(200).json({
+      msg: `Se agrego al sistema el alumno nuevo con el legajo n: ${newLegajo}`,
+      alumnoNuevo: alumnoNuevo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: `No se puedo dar de alta el nuevo alumno`,
+    });
+  } 
+};*/
+
+module.exports = { getAlumnoAll, getAlumnoById /*postAlumno*/ };
