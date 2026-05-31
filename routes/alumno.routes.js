20532caa3247e9fe2203f380a2839fd82c3b9366
Alan <alanhansenaxel@hotmail.com>
2026-05-31 06:53:18 -0300

    inicio de put alumno id

--- routes/alumno.routes.js
+++ routes/alumno.routes.js
@@ -2,11 +2,16 @@ const { Router } = require("express");
 const {
   getAlumnoAll,
   getAlumnoById,
+  updateAlumno,
 } = require("../controllers/alumno.controller");
 
 const rutas = Router();
 
+
 rutas.get("/", getAlumnoAll);
 rutas.get("/:legajo", getAlumnoById);
+rutas.put("/:legajo", updateAlumno);
 
 module.exports = rutas;
+
+
